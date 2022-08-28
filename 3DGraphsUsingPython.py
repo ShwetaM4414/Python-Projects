25c3cd5a63bdca6776fe3550689b7a7d6db36d96
ShwetaM4414 <82380372+ShwetaM4414@users.noreply.github.com>
2022-08-28 12:07:10 +0530

    Update 3DGraphsUsingPython.py

--- 3DGraphsUsingPython.py
+++ 3DGraphsUsingPython.py
@@ -1,113 +1,113 @@
 # 1. Line Plots
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-#
-# # Prepare arrays x,y,z
-#
-# theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-# z = np.linspace(-2, 2, 100)
-# r = z ** 2 + 1
-# x = r * np.sin(theta)
-# y = r * np.cos(theta)
-#
-# ax.plot(x, y, z, label='parametric curve')
-# ax.set_xlabel("X")
-# ax.set_ylabel("Y")
-# ax.set_zlabel("Z")
-# ax.legend()
-#
-# plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+# Prepare arrays x,y,z
+
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z ** 2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+
+ax.plot(x, y, z, label='parametric curve')
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+ax.legend()
+
+plt.show()
 
 
 # 2. Scatter Plots
 
 # import matplotlib.pyplot as plt
 # import numpy as np
-#
-# # Fixing random state for reproducibility
-#
-# np.random.seed(19680801)
-#
-#
-# def randrange(n, vmin, vmax):
-#     """
-#     function to make an array of random numbers having shape (n, )
-#     with each number distributed Uniform(vmin, vmax).
-#     """
-#
-#     return (vmax - vmin) * np.random.rand(n) + vmin
-#
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-#
-# n = 100
-#
-# # For each set of style and range settings, plot n random points in the box
-# # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-# for m, zlow, zhigh in [('o', -50, -25), ('x', -30, -5)]:
-#     xs = randrange(n, 23, 32)
-#     ys = randrange(n, 0, 100)
-#     zs = randrange(n, zlow, zhigh)
-#     ax.scatter(xs, ys, zs, marker=m)
-#
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-#
-# plt.show()
+
+# Fixing random state for reproducibility
+
+np.random.seed(19680801)
+
+
+def randrange(n, vmin, vmax):
+    """
+    function to make an array of random numbers having shape (n, )
+    with each number distributed Uniform(vmin, vmax).
+    """
+
+    return (vmax - vmin) * np.random.rand(n) + vmin
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+n = 100
+
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+for m, zlow, zhigh in [('o', -50, -25), ('x', -30, -5)]:
+    xs = randrange(n, 23, 32)
+    ys = randrange(n, 0, 100)
+    zs = randrange(n, zlow, zhigh)
+    ax.scatter(xs, ys, zs, marker=m)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
 
 
 # 3.Wireframe plots
-#
+
 # from mpl_toolkits.mplot3d import axes3d
 # import matplotlib.pyplot as plt
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-#
-# # Grab some test data.
-# X, Y, Z = axes3d.get_test_data(0.05)
-#
-# # Plot a basic wireframe.
-# ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5, color='black')
-# ax.set_title('Wireframe Plots')
-#
-# plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+# Grab some test data.
+X, Y, Z = axes3d.get_test_data(0.05)
+
+# Plot a basic wireframe.
+ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5, color='black')
+ax.set_title('Wireframe Plots')
+
+plt.show()
 
 # # 4.Surface Plot
-# #
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# from matplotlib.ticker import LinearLocator
-# import numpy as np
-#
-# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-#
-# # Make data.
-# X = np.arange(-5, 5, 0.25)
-# Y = np.arange(-5, 5, 0.25)
-# X, Y = np.meshgrid(X, Y)
-# R = np.sqrt(X**2 + Y**2)
-# Z = np.sin(R)
-#
-# # Plot the surface.
-# surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-#                        linewidth=0, antialiased=False)
-#
-# # Customize the z axis.
-# ax.set_zlim(-1.01, 1.01)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# # A StrMethodFormatter is used automatically
-# ax.zaxis.set_major_formatter('{x:.02f}')
-#
-# # Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-#
-# plt.show()
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
 
 # 5.Tri-Surface plots
 
@@ -119,8 +119,8 @@
 # Plot a 3D surface with a triangular mesh.
 # """
 #
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 
 n_radii = 8

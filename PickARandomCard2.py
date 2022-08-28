d345b1a372963c91ace191c3ad2240f64f3633e5
ShwetaM4414 <82380372+ShwetaM4414@users.noreply.github.com>
2022-08-28 12:21:18 +0530

    Add files via upload

---
+++ PickARandomCard2.py
@@ -0,0 +1,28 @@
+import random
+
+suit = ['Spade', 'Heart', 'Club', 'Diamond']
+rank = ['Ace', 'King', 'Queen', 'Joker', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+
+def PickACard():
+    Suit = random.choice(suit)
+    Rank = random.choice(rank)
+    Cards = Suit + ' ' + Rank
+    print(Cards)
+
+
+a = 1
+while a == 1:
+    try:
+        x = int(input("How many playing cards you want to pick : "))
+        while x > 52:
+            print("Value must be less than or equal to 52, Try again\n")
+            x = int(input("How many playing cards you want to pick : "))
+        a = 0
+
+    except ValueError:
+        print("Invalid Input, Try again\n")
+        a = 1
+    else:
+        for i in range(x):
+            PickACard()
